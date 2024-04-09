d34a91840c0fe56f24003bbc79b5c3763647ceba
Ali <work.gh.2021@gmail.com>
2024-04-09 19:20:14 +0330

    navbar & sidebar & page layout

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@ $(document).ready(function () {
     });
 
 });
+
+
+// ==================== navbar toggler ====================
+
+(function set_toggler_attr() {
+    const toggler = document.querySelector("#nav-toggle-icon");
+    if (window.innerWidth < 992) {
+        toggler.setAttribute("data-bs-toggle", "offcanvas");
+        toggler.setAttribute("data-bs-target", "#offcanvasRight");
+    } else {
+        toggler.setAttribute("data-bs-toggle", "collapse");
+        toggler.setAttribute("data-bs-target", "#sidebarCollapse");
+    }
+})();
